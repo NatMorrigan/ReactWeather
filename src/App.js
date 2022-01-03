2b92912ec6d255562fd9a38425f66be09e4a8488
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2022-01-03 16:55:43 +0100

    changed  html of app.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import React from "react";
 import "./App.css";
 
 
-import "bootstrap/dist/css/bootstrap.css";
+
 import WeatherSearch from "./WeatherSearch";
 import GitHubLink from "./GitHubLink";
 
 export default function App() {
   return (
-    <><div className="App">   
+    <div className="App">   
+    <div className = "container">
     <div className="input-group mb-3">
         {" "}
         <WeatherSearch />{" "}
     </div>
     </div><div className="Links">
         <footer>
-          This project is created by Natalia Hertsovska and is open-sourced on         <GitHubLink />
+          This project is created by {" "} <a href="https://www.linkedin.com/in/natalie-hertsovska-70976721a/" target="_blank">Natalia Hertsovska</a> {" "} and is {" "}open-sourced on <GitHubLink />
         </footer>
-      </div></>
+        </div>
+      </div>
   );
 
 }
