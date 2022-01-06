390cf92291d62ec4125909b3a8a7d74b8d96ccab
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2022-01-06 11:28:18 +0100

    deleted folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
 
-import React from "react";
-import "./App.css";
-
-import GitHubLink from "./GitHubLink";
-
-export default function App() {
-  return (
-    <div className="App">   
-   <div className="Links">
-        <footer>
-          This project is created by {" "} <a href="https://www.linkedin.com/in/natalie-hertsovska-70976721a/" target="_blank">Natalia Hertsovska</a> {" "} and is {" "}open-sourced on <GitHubLink />
-        </footer>
-        </div>
-      </div>
-  );
-}
