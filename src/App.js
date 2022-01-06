9a48fc2d1183d9c8534fe4d0527a02ca7823d430
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2022-01-06 12:17:05 +0100

    added link to git repo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./App.css";
 
 
 
-function App () {
-return <div className="App">Weather App</div>    
+export default function App () {
+return <div className="App">
+ <h1>Weather App</h1>
+    <footer>
+This project was created by Natalia Hertsovska and is {" "} 
+<a href="https://github.com/NatMorrigan/ReactWeather" target="_blank">
+open-sourced on GitHub
+</a> 
+</footer>
+</div>
 }
 
 
-export default App;
