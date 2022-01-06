57a13629a13ec659d841b2f75ec83bc6967a01ed
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2022-01-06 12:29:50 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import "./App.css";
 
 export default function App () {
 return <div className="App">
+    <div className="container">
  <h1>Weather App</h1>
     <footer>
 This project was created by Natalia Hertsovska and is {" "} 
-<a href="https://github.com/NatMorrigan/ReactWeather" target="_blank">
+<a href="https://github.com/NatMorrigan/ReactWeather" target="_blank" rel="noreferrer">
 open-sourced on GitHub
 </a> 
-</footer>
-</div>
+   </footer>
+    </div>
+     </div>
 }
 
 
